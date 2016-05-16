dc541048cb804bd1225b421d899cac685c5fd92f
Ian Ross <iross@cadasta.org>
2016-05-16 09:49:36 +0200

    Generalise instance assignment for fields

--- jsonattrs/decorators.py
+++ jsonattrs/decorators.py
@@ -1,9 +1,13 @@
 from django.db.models.signals import post_init
 
+from .fields import JSONAttributes
+
 
 def fixup_instance(sender, **kwargs):
     instance = kwargs['instance']
-    instance.attrs._instance = instance
+    for k, v in instance.__dict__.items():
+        if isinstance(v, JSONAttributes):
+            v._instance = instance
 
 
 def fix_model_for_attributes(cls):

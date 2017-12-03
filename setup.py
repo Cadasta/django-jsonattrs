d6061a19cade2e46c22172fcfa51063477afc904
requires.io <support@requires.io>
2017-12-02 18:55:57 -0700

    [requires.io] dependency update

--- setup.py
+++ setup.py
@@ -84,7 +84,7 @@ setup(
     packages=get_packages(package),
     package_data=get_package_data(package),
     install_requires=[
-        'Django==1.11.8',
+        'Django==2.0',
         'django-audit-log==0.7.0'
     ],
     classifiers=[
